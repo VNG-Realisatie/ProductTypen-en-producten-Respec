7f07758456426d3bed2ad4101292fa96453bd83f
RonaldJobse <ronald.jobse@vng.nl>
2026-07-08 09:51:00 +0200

    v0.0.1

--- js/config.js
+++ js/config.js
@@ -2,26 +2,44 @@ let respecConfig = {
 //  useLogo: true,
 //  useLabel: true,
 //  license: "eupl",
-  shortName: "Respec-template",
-  pubDomain: "hl",
+  shortName: "Producttypen en producten register",
+  pubDomain: "cim",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-types
-  specType: "HL",
+  specType: "IM",
   
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane specificatie-statussen
   specStatus: "IO",
-  publishDate: "2026-05-19",
-  publishVersion: "0.3.0",
+  publishDate: "2026-07-08",
+  publishVersion: "0.0.1",
 
   // Zie de globale property 'localizationStrings/nl' voor de lijst met toegestane maturities
   //previousMaturity: "IO",
   //previousPublishDate: "2022-06-24",
   //previousPublishVersion: "0.0.4",
   
-  title: "Respec documentatie generatie",
-  subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
-  content: {"mermaid": "", "ch01": "informative", "ch02": ""},
-//  authors:
+  title: "Informatiemodel Producttypen en producten register",
+  // subtitle: "Tevens een voorbeeld van een gegenereerd Respec document",
+  // content: {"mermaid": "", "ch01": "informative", "ch02": ""},
+  authors:
+    [
+      {
+        name: "Jan Brekelmans",
+        company: "Gemeente Den Haag",
+        companyURL: "https://www.denhaag.nl",
+      },
+    ],
+
+  editors:
+    [
+      {
+        name: "Ronald Jobse",
+        company: "VNG Realisatie",
+        companyURL: "https://vng.nl/artikelen/vng-realisatie",
+      }
+    ],
+
+  //  editors:
 //    [
 //      {
 //        name: "Robert Melskens",
@@ -29,38 +47,30 @@ let respecConfig = {
 //        companyURL: "https://vng.nl/artikelen/vng-realisatie",
 //      }
 //    ],
-  editors:
-    [
-      {
-        name: "Robert Melskens",
-        company: "VNG Realisatie",
-        companyURL: "https://vng.nl/artikelen/vng-realisatie",
-      }
-    ],
-  github: "https://github.com/VNG-Realisatie/VNG-R-Respec-Template",
+  github: "https://github.com/VNG-Realisatie/ProductTypen-en-producten-Respec",
 
-  //  maxTocLevel: 2,
+//    maxTocLevel: 2,
 
   // Creëer PDF en link deze aan de file in de header van het html document (optioneel). Het is (nog) niet mogelijk hier een globale property van te maken:
 //  alternateFormats: [
 //      {
 //          label: "pdf",
-//          uri: "hl-Respec-template-0.2.0.pdf",
+//          uri: "im-ProductTypen-en-producten-register-0.0.1.pdf",
 //      },
 //    ],
-  localBiblio: {
-        "MIM": {
-           "href": "https://docs.geostandaarden.nl/mim/mim/",
-           "publisher": "Geonovum",
-           "title": "MIM - Metamodel Informatie Modellering",
-           "date": "Oktober 2023",
-           "rawDate": "2023"
-        },
-        "SemVer": {
-           "href": "https://semver.org/lang/nl/",
-           "title": "Semantisch Versioneren 2.0.0",
-           "date": "December 19, 2023",
-           "rawDate": "2023"
-        },
-    },
+//  localBiblio: {
+//        "MIM": {
+//           "href": "https://docs.geostandaarden.nl/mim/mim/",
+//           "publisher": "Geonovum",
+//           "title": "MIM - Metamodel Informatie Modellering",
+//           "date": "Oktober 2023",
+//           "rawDate": "2023"
+//        },
+//        "SemVer": {
+//           "href": "https://semver.org/lang/nl/",
+//           "title": "Semantisch Versioneren 2.0.0",
+//           "date": "December 19, 2023",
+//           "rawDate": "2023"
+//        },
+//    },
 }
